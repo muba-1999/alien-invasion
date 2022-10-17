f0bd3bb60271bedf9975e508fc61dc09ed0a229b
mubanga <mukobam4@gmail.com>
2022-10-17 19:54:46 +0200

    loaded the ship image and set its position

--- ship.py
+++ ship.py
@@ -3,5 +3,20 @@ import pygame
 class Ship:
 	"""Class that mmanages the ships behavior"""
 
-	def __init__(self):
+	def __init__(self, ai_game):
 		"""Initailize the ship and set its starting position"""
+
+		self.screen = ai_game.screen
+		self.screen_rect = ai_game.screen.get_rect()
+
+		#loading ship image and getting its rect
+		self.image = pygame.image.load('./images/ship.bmp')
+		self.rect = self.image.get_rect()
+
+		#start each ship at the middle bottom
+		self.rect.midbottom = self.screen_rect.midbottom
+
+
+	def blitme(self):
+		#Draw the ship at its current position
+		self.screen.blit(self.image, self.rect)
