babf8c177fbbd5b6b49b15d77c50e76a1031eb85
mubanga <mukobam4@gmail.com>
2022-10-17 23:09:16 +0200

    added logic to prevent the ship from going out of bounds

--- ship.py
+++ ship.py
@@ -19,13 +19,20 @@ class Ship:
 		self.moving_right = False
 		self.moving_left = False
 
+		self.settings = ai_game.settings
+
+		self.x = float(self.rect.x)
+
 	def update(self):
 		"""updates the ships position base on the movement flag"""
 
-		if self.moving_right:
-			self.rect.x += 1
-		if self.moving_left:
-			self.rect.x -= 1
+		if self.moving_right and self.rect.right < self.screen_rect.right:
+			self.x += self.settings.ship_speed
+		if self.moving_left and self.rect.left > 0:
+			self.x -= self.settings.ship_speed
+
+		#updating rect object from self.x
+		self.rect.x = self.x
 
 
 	def blitme(self):
