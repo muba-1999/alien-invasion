ecb203c74aa37deede4c0cc95e3f3763d4bc18eb
mubanga <mukobam4@gmail.com>
2022-10-17 19:22:26 +0200

    added the secreen width and height variables and the backgroud color

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+	"""class to store settings for alien invasion"""
+
+	def __init__(self):
+		"""initializing the game seetings"""
+
+		#screen settings
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.bg_color = (230, 230, 230)
