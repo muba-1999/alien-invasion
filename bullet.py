f85303a6a4ff664b899583015955ccf0a66b2c90
mubanga <mukobam4@gmail.com>
2022-10-19 11:24:54 +0200

    created the update method to control the position of the bullets

--- bullet.py
+++ bullet.py
@@ -10,10 +10,26 @@ class Bullet(Sprite):
 
 		self.screen = ai_game.screen
 		self.settings = ai_game.settings
-		self.color = settings.bullet_color
+		self.color = self.settings.bullet_color
 
 		#creating a bullet rect and setting its position
 		self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
 		self.rect.midtop = ai_game.ship.rect.midtop
 
 		self.y = float(self.rect.y)
+
+
+	def update(self):
+		"""moving the bullet up the screen"""
+
+		#uodating the position of the bullet
+		self.y -= self.settings.bullet_speed
+
+		#updating the rect position 
+		self.rect.y = self.y
+
+
+	def draw_bullet(self):
+		"""drawing the bullet"""
+
+		pygame.draw.rect(self.screen, self.color, self.rect)
