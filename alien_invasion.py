8d4a0b2e5a5f69c4b6e0d6afc1e5627410339fb7
mubanga <mukobam4@gmail.com>
2022-10-19 19:29:16 +0200

    added a parameter to _create_alien helper

--- alien_invasion.py
+++ alien_invasion.py
@@ -50,10 +50,10 @@ class AlienInvasion:
 
 		#creating the first row of aliens
 		for alien_number in range(number_aliens_x):
-			self._create_alien()
+			self._create_alien(alien_number)
 
 
-	def _create_alien(self):
+	def _create_alien(self, alien_number):
 		#creates an alien
 		alien = Alien(self)
 		alien.x = alien_width + 2 * alien_width * alien_number
