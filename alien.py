473e102db7d36aea7183e111e15445b8ce142683
mubanga <mukobam4@gmail.com>
2022-10-19 18:43:03 +0200

    create the alien.py file and loaded the alien image and set its position

---
+++ alien.py
@@ -0,0 +1,25 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Alien(Sprite):
+	"""class creates and controls alien behavior"""
+
+	def __init__(self, ai_game):
+		"""initializing an alien and setting its start position"""
+
+		super().__init__()
+		self.screen = ai_game.screen
+
+		#loading the alien image and setting its rect
+		self.image = pygame.image.load('./images/alien.bmp')
+		self.rect = self.image.rect_get()
+
+		#start each alien near the top left of the screen
+		self.rect.x = self.rect.width
+		self.rect.y = self.rect.height
+
+		#storing the aliens horizontal position
+		self.x = float(self.rect.x)
+
+
